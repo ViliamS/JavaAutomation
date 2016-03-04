2070e26a357dba9b0bfe45dcee9bb447cd6912da
Anthony Mottot <anthony.mottot@abakus.com>
2016-03-04 15:58:07 +0100

    explicit null parameters for User constructor as Jenkins is failing

--- Apollo/src/main/java/com/r2development/leveris/di/ApolloDependenciesModule.java
+++ Apollo/src/main/java/com/r2development/leveris/di/ApolloDependenciesModule.java
@@ -104,7 +104,7 @@ public class ApolloDependenciesModule extends AbstractModule {
         }
 //        bind(User.class).toInstance(user);
         if ( user == null)
-            user = new User();
+            user = new User(null, null, null, null);
         bind(IUser.class).toInstance(user);
     }
 }
