f9093800f216ba271572519ea7d3c1d6e492496d
Anthony Mottot <anthony.mottot@abakus.com>
2016-03-24 11:50:29 +0100

    To validate jenkins settings : override tags and root features resources

--- Testing/src/test/java/com/r2development/leveris/bdd/borrower/runner/api/borrower/RunnerTest.java
+++ Testing/src/test/java/com/r2development/leveris/bdd/borrower/runner/api/borrower/RunnerTest.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 "json:target/Runner-Cucumber-Json/Runner.json"
         },
         strict = true,
-        features = "classpath:bdd/features/borrower/api/borrower/",
+        features = "classpath:bdd/features/",
         tags = "@Payday"
 )
 public class RunnerTest {
